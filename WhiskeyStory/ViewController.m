838f647226052f40fc9f93da780ebe14e84315e8
Derek316x <ThePro@Zs-MacBook-Pro.local>
2015-06-08 00:10:23 -0400

    before i changed code structure

--- WhiskeyStory/ViewController.m
+++ WhiskeyStory/ViewController.m
@@ -9,44 +9,67 @@
 #import "ViewController.h"
 
 //do we want to party?
-BOOL whiskeyMood;
+BOOL whiskeyMood = NO;
 
-BOOL shots;
-BOOL neat;
-BOOL cocktail;
+BOOL shots = NO;
+BOOL neat = NO;
+BOOL cocktail =NO;
 
 //shot bools
-BOOL wasted;
-BOOL buzzed;
+BOOL wasted = NO;
+BOOL buzzed = NO;
 
 //cocktail bools
-BOOL shaken;
-BOOL stirred;
+BOOL shaken = NO;
+BOOL stirred = NO;
 
 // ints for switch case statements
-int drinkType;
-int flavor;
+int drinkType = 0;
+int flavor = 0;
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *currentQuestion;
+@property (weak, nonatomic) IBOutlet UIButton *moodYES;
+@property (weak, nonatomic) IBOutlet UIButton *moodNO;
+@property (weak, nonatomic) IBOutlet UIButton *neatButton;
+@property (weak, nonatomic) IBOutlet UIButton *shotButton;
+@property (weak, nonatomic) IBOutlet UIButton *cocktailButton;
 
 
 @end
 
 @implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self hideDrinkType];
+    
+    self.currentQuestion.text = @"Are you in the mood for whiskey?";
+    
+}
+
 - (IBAction)moodYES:(id)sender {
     
-    whiskeyMood = YES;
+    //hide YES/NO
+    [self hideYesNo];
     
+    whiskeyMood = YES;
     if (whiskeyMood) {
         
             //user chooses shot, neat, or cocktail
             self.currentQuestion.text = @"Select your drink type.";
         
+        [self showDrinkType];
+        
         switch (drinkType) {
                 
             case 1: //shots
                 shots = YES;
+                [self hideDrinkType];
+                
+                self.currentQuestion.text = @"How hard are you going tonight?.";
+                
                 if (wasted) {
                     //SHOTS SHOTS SHOTS
                 }
@@ -57,6 +80,7 @@ int flavor;
                 break;
             case 2: //neat
                 neat = YES;
+                [self hideDrinkType];
                 
                 switch (flavor) {
                     case 1: //sweet
@@ -73,6 +97,7 @@ int flavor;
                 break;
             case 3: //cocktail
                 cocktail = YES;
+                [self hideDrinkType];
                 if (shaken) {
                     //shake drink
                 }
@@ -87,18 +112,47 @@ int flavor;
 }
 - (IBAction)moodNO:(id)sender {
     
+    //hide YES/NO
+    [self hideYesNo];
+    
     //no whiskey tonight
     whiskeyMood = NO;
     self.currentQuestion.text = @"Don't order whiskey.";
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    
-    self.currentQuestion.text = @"Are you in the mood for whiskey?";
-    
+
+//sets drink types when button is pressed
+- (IBAction)shotButton:(id)sender {
+    drinkType=1;
 }
 
+- (IBAction)neatButton:(id)sender {
+    drinkType=2;
+}
+- (IBAction)cocktailButton:(id)sender {
+    drinkType=3;
+}
+
+//methods to show and hide buttons
+-(void)hideYesNo {
+    self.moodNO.hidden=YES;
+    self.moodYES.hidden=YES;
+}
+
+-(void)hideDrinkType {
+    self.neatButton.hidden=YES;
+    self.shotButton.hidden=YES;
+    self.cocktailButton.hidden=YES;
+}
+
+-(void)showDrinkType {
+    self.neatButton.hidden=NO;
+    self.shotButton.hidden=NO;
+    self.cocktailButton.hidden=NO;
+}
+
+-(void)drinkTypeSwitcher {
+    
+}
 
 @end
